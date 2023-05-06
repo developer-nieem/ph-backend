a431e3aba706025cf7c8374c1a89e9db4119e815
developer-nieem <naimhossenwork@gmail.com>
2023-05-06 20:25:00 +0600

    added some updated

--- users-management-server/index.js
+++ users-management-server/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port =  process.env.PORT ||  3000
+
+
+app.use(cors())
+app.use(express.json());
+
+const users = [
+    { id: 1, name: 'Sabana', email: 'sabana@gmail.com' },
+    { id: 2, name: 'Sabnoon', email: 'sabnoon@gmail.com' },
+    { id: 3, name: 'Sabila', email: 'sabila@gmail.com' },
+]
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
