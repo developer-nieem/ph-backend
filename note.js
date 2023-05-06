8f5e9a5c152c5a55e1e929f28c5a86b26ce85e49
developer-nieem <naimhossenwork@gmail.com>
2023-05-06 20:20:29 +0600

    added client manneg server

---
+++ note.js
@@ -0,0 +1,22 @@
+// NodeJS vs Express
+
+// Feature  ---  ExpressJs  ----------  NodeJS
+// Purpose  --- Web framework  --  JS Runtime
+// Built on  --- NodeJs  -------  Chrome's V8 JavaScript engine
+// Focus  ---- Server-side development  --- General-purpose JavaScript programming
+// Key features  ---  Routing, middleware, tempting  -- Non-blocking I/O, event-driven  programming
+// Use Cases  -- Building web applications, RESTful APIs  --- Server-side scripting, creating command line tools
+
+
+// RDBMS vs  MongoDB  
+
+// Database   ------  Database
+// Tables       collections
+// Rows      Documents
+// Columns     Fields
+
+
+
+
+
+
